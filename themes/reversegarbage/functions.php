ae15550909f47abd2621241a2cdc8e0f2ff6e43f
Trent McClenahan <trent.mcclenahan@gmail.com>
2011-03-30 22:10:29 +0200

    initial theme using twentyten template that ships with WP 3.1

---
+++ themes/reversegarbage/functions.php
@@ -0,0 +1,22 @@
+<?php
+function reversegarbage_widgets_init() {
+	// Area 1, located at the top of the sidebar.
+	register_sidebar( array(
+		'name' => __( 'Main Widget Area', 'reversegarbage' ),
+		'id' => 'main-widget-area',
+		'description' => __( 'The main widget area', 'reversegarbage' ),
+		'before_widget' => '<div id="%1$s" class="widget-container %2$s">',
+		'after_widget' => '</div>',
+		'before_title' => '<h3 class="widget-title">',
+		'after_title' => '</h3>',
+	) );
+}
+// Register sidebars by running twentyten_widgets_init() on the widgets_init hook.
+add_action( 'widgets_init', 'reversegarbage_widgets_init' );
+
+function my_unregister_sidebars() {
+	unregister_sidebar( 'third-footer-widget-area' );
+	unregister_sidebar( 'fourth-footer-widget-area' );
+}
+// unregister sidebars by tying onto same hook again, with priority '11'
+add_action( 'widgets_init', 'my_unregister_sidebars', 11 );
---
+++ themes/reversegarbage/sidebar-main.php
@@ -0,0 +1,7 @@
+<?php if ( is_active_sidebar( 'main-widget-area' ) ) : ?>
+				<div id="main" class="widget-area">
+					<ul class="xoxo">
+						<?php dynamic_sidebar( 'main-widget-area' ); ?>
+					</ul>
+				</div><!-- #first .widget-area -->
+<?php endif; ?>
